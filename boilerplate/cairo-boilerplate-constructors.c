8dd7bc6b4ed39fa2a97737e2ffebe58efe4c7965
Vincent Wei <vincent@minigui.org>
2019-11-07 11:27:46 +0800

    change version to 1.17.3; change sonum to 3

--- boilerplate/cairo-boilerplate-constructors.c
+++ boilerplate/cairo-boilerplate-constructors.c
@@ -7,8 +7,6 @@ void _cairo_boilerplate_register_all (void);
 extern void _register_builtin (void);
 extern void _register_xlib (void);
 extern void _register_drm (void);
-extern void _register_egl (void);
-extern void _register_gl (void);
 extern void _register_script (void);
 extern void _register_ps (void);
 extern void _register_pdf (void);
@@ -20,8 +18,6 @@ _cairo_boilerplate_register_all (void)
     _register_builtin ();
     _register_xlib ();
     _register_drm ();
-    _register_egl ();
-    _register_gl ();
     _register_script ();
     _register_ps ();
     _register_pdf ();
